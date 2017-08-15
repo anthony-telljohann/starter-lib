f1cab53609835897ff66f067d31fa4dd34d2804c
Anthony Telljohann-Smothers <anthony@telljohann-smothers.com>
2017-08-14 21:10:01 -0400

    :nut_and_bolt: (rollup.config) reduce complexity

--- rollup.config.js
+++ rollup.config.js
@@ -2,26 +2,18 @@ import babel from 'rollup-plugin-babel'
 import pkg from './package.json'
 import prettier from 'rollup-plugin-prettier'
 
-const entry = 'lib/starter-lib.js'
-const external = Object.keys(pkg.dependencies)
-const plugins = [babel(), prettier(pkg.prettier)]
-
 let dest
-let format
 
-if (process.env.BABEL_ENV === 'cjs') {
-  dest = pkg.main
-  format = 'cjs'
-}
 if (process.env.BABEL_ENV === 'es') {
   dest = pkg.module
-  format = 'es'
+} else {
+  dest = pkg.main
 }
 
 export default {
-  format,
-  entry,
-  dest,
-  external,
-  plugins
+  format: process.env.BABEL_ENV,
+  entry: `lib/starter-lib.js`,
+  dest: dest,
+  external: Object.keys(pkg.dependencies),
+  plugins: [babel(), prettier(pkg.prettier)]
 }
