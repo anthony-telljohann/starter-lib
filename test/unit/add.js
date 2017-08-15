83e54391ab3dbe52557fb27335e4c9565cf3e2cb
Anthony Telljohann-Smothers <anthony@telljohann-smothers.com>
2017-08-15 00:23:22 -0400

    test(unit): add unit tests

---
+++ test/unit/add.js
@@ -0,0 +1,19 @@
+import add from 'add'
+import is from 'is'
+import test from 'ava'
+
+test('is a function', async t => {
+  t.true(is.fn(add))
+})
+
+test('returns a Promise', async t => {
+  t.true(is.instance(add(), Promise))
+})
+
+test('resolves a Number', async t => {
+  t.true(is.number(await add(1, 1)))
+})
+
+test('throws TypeError', async t => {
+  await t.throws(add({}, {}))
+})
