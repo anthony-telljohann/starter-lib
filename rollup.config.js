d1062363c062db7fd2553ba71bdc8ad927e0d79c
Anthony Telljohann-Smothers <anthony@telljohann-smothers.com>
2017-08-14 20:11:29 -0400

    update rollup.config.js

--- rollup.config.js
+++ rollup.config.js
@@ -1,27 +1,20 @@
+import alias from 'rollup-plugin-alias'
 import babel from 'rollup-plugin-babel'
 import pkg from './package.json'
 import prettier from 'rollup-plugin-prettier'
 
-const entry = 'lib/starter-lib.js'
-const external = Object.keys(pkg.dependencies)
-const plugins = [babel(), prettier(pkg.prettier)]
-
 let dest
-let format
 
-if (process.env.BABEL_ENV === 'cjs') {
-  dest = pkg.main
-  format = 'cjs'
-}
 if (process.env.BABEL_ENV === 'es') {
   dest = pkg.module
-  format = 'es'
+} else {
+  dest = pkg.main
 }
 
 export default {
-  format,
-  entry,
-  dest,
-  external,
-  plugins
+  format: process.env.BABEL_ENV,
+  entry: `lib/${pkg.name}.js`,
+  dest: dest,
+  external: Object.keys(pkg.dependencies),
+  plugins: [alias(pkg.rollup.alias), babel(), prettier(pkg.prettier)]
 }
